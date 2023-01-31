a283595257a70c615dbb10eb4878abcb0e4a56d4
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-01-31 21:18:48 +0100

    update

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,19 @@
-print("Hello, World!")
+
+# first program
 
 print("Hello, World!")
+
+# second program
 
 if 5 > 2:
-    print("Five is greater than two")
+ print("Five is greater than two!") 
+if 5 > 2:
+     print("Five is greater than two!") 
+
+x = 5
+y = "Hello, World!"
+print(x)
+
+x = 6
+y = 5
+print(6 * 5)
