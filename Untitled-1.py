d6d18c9d362a8bf0666160e3c7ed9dccf0a85422
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-02-01 19:00:11 +0100

    update

--- Untitled-1.py
+++ Untitled-1.py
@@ -19,7 +19,6 @@ y = 5
 print(6 * 5)
 
 x = 5
-
 y = 6
 
 a = ('John', 'Charles', 'Mike')
@@ -28,14 +27,31 @@ x = zip(a, b)
 #use the tuple() function to display a readable version of the result:
 print(tuple(x))
 
-
 #Band Name Generator
-
 
 print ("Welcome to the Band Name Generator")
 street = input("Whats is the name of the city you grew up in ?\n")
 pet = input("What is your pet is name?\n")
 print ("Your band name could be " + street + " " + pet)
+
+print("Day 1 - Python Print Function")
+
+print("Day 1 - Python Print Function\nHello world")
+
+print ("test\ntest\ntest\ntest")
+
+print("Hello" + " " + "Patryk")
+
+print("Hello " + "Patryk")
+
+print("Hello" + " Patryk")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+"sign.")
+print ("e.g. print Hello" + " world")
+print ("New lines can be created with a backslash and n.\n")
+
+
 
 
 
