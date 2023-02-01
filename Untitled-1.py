5d7ff91de4b42f47263aab9829ce5dac5920027f
Patryk-Kazmierczak <patrykkazmierczak0@gmail.com>
2023-02-01 10:45:34 +0100

    update

--- Untitled-1.py
+++ Untitled-1.py
@@ -17,3 +17,27 @@ print(x)
 x = 6
 y = 5
 print(6 * 5)
+
+x = 5
+
+y = 6
+
+a = ('John', 'Charles', 'Mike')
+b = ('Jenny', 'Christy', 'Monica')
+x = zip(a, b)
+#use the tuple() function to display a readable version of the result:
+print(tuple(x))
+
+
+#Band Name Generator
+
+
+print ("Welcome to the Band Name Generator")
+street = input("Whats is the name of the city you grew up in ?\n")
+pet = input("What is your pet is name?\n")
+print ("Your band name could be " + street + " " + pet)
+
+
+
+
+
