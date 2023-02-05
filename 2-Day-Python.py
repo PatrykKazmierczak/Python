d82db025bba25cebcb627736e36312c403bbd1d6
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-02-05 22:42:36 +0100

    update

--- 2-Day-Python.py
+++ 2-Day-Python.py
@@ -99,6 +99,39 @@ bmi = int(weight) / float(height) ** 2
 bmi_as_int = int(bmi)
 print (round(bmi_as_int, 3))
 
+print(8 / 3)
+
+print(round(8 / 3, 2))
+
+print(round(8 / 3, 4))
+
+result  = 4 / 2
+result /= 2
+print(result)
+
+score = 13
+score += 1
+print(score)
+
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+
+age = input("What is your age")
+
+age_as_int = int(age)
+
+years_remaining = 90 - age_as_int
+
+days_remaining = years_remaining * 365
+weeks_remaining= years_remaining * 52
+months_remaining = years_remaining * 12
+
+
+message = (f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left")
+print(message)
 
 
 
