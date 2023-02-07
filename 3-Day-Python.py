c5c223541c7523f83ba86b99d692c43d0e631d5f
Patryk-Kazmierczak <patrykkazmierczak0@gmail.com>
2023-02-07 17:35:17 +0100

    Update

--- 3-Day-Python.py
+++ 3-Day-Python.py
@@ -92,6 +92,29 @@ if year % 4 == 0:
 else:
     print("Not leap year")
 
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height? "))
+bill = 0
+
+if height >= 120:
+    print("You can ride rollercoaster.")
+    age = int(input("How old are you? "))
+    if age < 12:
+        bill = 5
+        print("Child tickets are 5 $.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are 7 $ ")
+    else:
+        bill = 18
+        print("Adult tickets are 18$ ")
+    wants_foto = input("Do you want to photo taken? Y or N.")
+    if wants_foto == "Y":
+        bill += 3
+    print(f"Your final bill is {bill}")
+else:
+    print("You have to grow taller before you can ride.")
+
 
 
 
