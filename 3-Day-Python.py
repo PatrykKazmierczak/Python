83de6293e1aab175e1b7d9601dcca19a6510cc88
Patryk-Kazmierczak <patrykkazmierczak0@gmail.com>
2023-02-08 17:55:07 +0100

    Update

--- 3-Day-Python.py
+++ 3-Day-Python.py
@@ -115,8 +115,58 @@ if height >= 120:
 else:
     print("You have to grow taller before you can ride.")
 
+print("Welcome to Phyton Pizza Deliveries")
+size = input("What size pizza do you want? S, M, L")
+add_pepperoni = input("Do you want pepperoni? Y or N")
+extra_cheese = input("Do you want extra cheese? Y or N")
+bill =0
+
+if size == "S":
+    bill = 15
+    if add_pepperoni == "Y":
+        bill += 2
+    if extra_cheese == "Y":
+        bill += 1
+    print(f"Your final bill is{bill}")
+if size == "M":
+    bill = 20
+    if add_pepperoni == "Y":
+        bill += 3
+    if extra_cheese == "Y":
+        bill += 1
+    print(f"Your final bill is{bill}")
+if size == "L":
+    bill = 25
+    if add_pepperoni == "Y":
+        bill += 3
+    if extra_cheese == "Y":
+        bill += 1
+    print(f"Your final bill is{bill}")
 
 
+print("Welcome to Phyton Pizza Deliveries")
+size = input("What size pizza do you want? S, M, L")
+add_pepperoni = input("Do you want pepperoni? Y or N")
+extra_cheese = input("Do you want extra cheese? Y or N")
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    if size == "S":
+        bill += 1
+
+print(f"Your final bill is{bill}")
 
 
 
