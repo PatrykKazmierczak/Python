2f6d36309b41b575b3c9167d7aac8b0f54c2ac3e
Patryk-Kazmierczak <patrykkazmierczak0@gmail.com>
2023-02-15 13:03:39 +0100

    Update

---
+++ 6-Day-Python.py
@@ -0,0 +1,68 @@
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+#
+# def my_funtion():
+#     print("Hello")
+#     print("Bye")
+#
+# my_funtion()
+from asyncore import loop
+
+
+def my_function():
+    if sky == "clear":
+        print("blue")
+    elif sky == "cloudy":
+        print("grey")
+    print("Hello")
+print("World")
+
+
+my_function()
+
+ i = 1
+ while i < 6:
+     print(i)
+     i += 2
+
+def loop_function():
+    i = 1
+    while i < 6:
+        print(i)
+        i += 1
+
+loop_function()
+
+def loop_function():
+    i = 1
+    while i < 6:
+        print(i)
+        i += 1
+    else:
+        print(f"{i} is no longer less than 6")
+
+loop_function()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
