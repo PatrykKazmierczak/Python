99007c857edcc0f03fe34020e5f4d897cd02e97a
Patryk-Kazmierczak <patrykkazmierczak0@gmail.com>
2023-03-09 11:05:44 +0100

    Update

--- 7-Day-Python.py
+++ 7-Day-Python.py
@@ -1,15 +1,42 @@
 
 
+# import random
+#
+# word_list = ["ardvark", "baboon", "camel"]
+#
+# chosen_word = random.choice(word_list)
+#
+# guess = input("Guess a letter:").lower()
+#
+# for letter in chosen_word:
+#     if letter == guess:
+#         print("Right")
+#     else:
+#         print("Wrong")
+
 import random
 
-word_list = ["ardvark", "baboon", "camel"]
+word_list = ["banana", "apple", "orange"]
+
+choice_word = random.choice(word_list)
+
+print(f"{choice_word}")
+
+
+import random
+
+word_list = ["banana", "apple", "orange"]
 
 chosen_word = random.choice(word_list)
 
-guess = input("Guess a letter:").lower()
+guess = input("Guess a letter: ").lower()
 
 for letter in chosen_word:
     if letter == guess:
         print("Right")
     else:
         print("Wrong")
+
+
+
+
